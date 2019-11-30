4207781fb3be13137db86a6121dbf581a3ff0ff3
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-30 13:47:47 -0500

    Create Player.gd

---
+++ Project/Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+var Velocity = Vector2()
+
+export (int) var speed = 500
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		Velocity.x = speed
+	elif Input.is_action_pressed("ui_left"):
+		Velocity.x = -speed
+	else:
+		Velocity.x = 0
+	if Input.is_action_pressed("ui_down"):
+		Velocity.y = speed
+	elif Input.is_action_pressed("ui_up"):
+		Velocity.y = -speed
+	else:
+		Velocity.y = 0
+	move_and_slide(Velocity)
+	$Sprite.look_at(get_global_mouse_position())
